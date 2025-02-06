4281f9bc0dbca1ef87ffd430311941b22cf9096e
Akash Giri <158684724+Egakash@users.noreply.github.com>
2025-02-06 21:18:40 +0530

    Add files via upload

---
+++ jiss.js
@@ -0,0 +1,6 @@
+// Function to show the personalized Rose Day message
+function showMessage() {
+    let messageContainer = document.getElementById("personal-message");
+    messageContainer.innerHTML = "Wishing you a day as bright and beautiful as a rose!";
+    messageContainer.style.display = "block";
+}
